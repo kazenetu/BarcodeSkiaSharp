6e6b1c80d33375a7e903e60586579669fc8507ee
kazenetu <rakudafactory@gmail.com>
2024-04-22 22:57:26 +0900

    ソースコード追加 #1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BarcodeSkiaSharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
